7676dcff8bc6aaf7431c2f7c3a1a6d3ac80f7536
Mate <mate@localhost.hu>
2023-09-19 14:19:58 +0200

    APOKDAKLSd

--- WAProject230919/FrmMain.Designer.cs
+++ WAProject230919/FrmMain.Designer.cs
@@ -29,13 +29,97 @@ namespace WAProject230919
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnTorles = new System.Windows.Forms.Button();
+            this.pbSadCat = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnRandom = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSadCat)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTorles
+            // 
+            this.btnTorles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTorles.BackColor = System.Drawing.Color.Red;
+            this.btnTorles.ForeColor = System.Drawing.Color.White;
+            this.btnTorles.Location = new System.Drawing.Point(811, 445);
+            this.btnTorles.Name = "btnTorles";
+            this.btnTorles.Size = new System.Drawing.Size(206, 61);
+            this.btnTorles.TabIndex = 0;
+            this.btnTorles.Text = "Törlés";
+            this.btnTorles.UseVisualStyleBackColor = false;
+            // 
+            // pbSadCat
+            // 
+            this.pbSadCat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbSadCat.BackColor = System.Drawing.Color.Transparent;
+            this.pbSadCat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbSadCat.Location = new System.Drawing.Point(204, 3);
+            this.pbSadCat.Name = "pbSadCat";
+            this.pbSadCat.Size = new System.Drawing.Size(597, 421);
+            this.pbSadCat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSadCat.TabIndex = 1;
+            this.pbSadCat.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.pbSadCat, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 427F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1005, 427);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRandom.BackColor = System.Drawing.Color.Green;
+            this.btnRandom.ForeColor = System.Drawing.Color.White;
+            this.btnRandom.Location = new System.Drawing.Point(12, 445);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(206, 61);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1029, 518);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.btnTorles);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmMain";
+            this.Text = "Random Sad Cat";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSadCat)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTorles;
+        private System.Windows.Forms.PictureBox pbSadCat;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnRandom;
     }
 }
 
--- WAProject230919/FrmMain.cs
+++ WAProject230919/FrmMain.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WAProject230919.Properties;
 
 namespace WAProject230919
 {
     public partial class FrmMain : Form
     {
+        static Random rnd = new Random();
+        private int clickCounter = 0;
+
+        static Image[] sadcats =
+        {
+            Resources.cat1,
+            Resources.cat2,
+            Resources.cat3,
+            Resources.cat4,
+            Resources.cat5
+        };
         public FrmMain()
         {
             InitializeComponent();
+            btnRandom.Click += BtnRandomClick;
+            btnTorles.Click += BtnTorlesClick;
+        }
+
+        private void BtnTorlesClick(object sender, EventArgs e)
+        {
+            pbSadCat.Image = null;
+        }
+
+        private void BtnRandomClick(object sender, EventArgs e)
+        {
+            pbSadCat.Image = sadcats[rnd.Next(sadcats.Length)];
         }
     }
 }
